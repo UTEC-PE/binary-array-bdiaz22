92bda9d44498b187fcb345323d50b32a14cd1c50
bryan <bryan.diaz@utec.edu.pe>
2018-10-30 22:41:08 -0500

    terminado binary array

--- binary.cpp
+++ binary.cpp
@@ -1,34 +1,68 @@
 #ifndef BOOLEAN_H
 #define BOOLEAN_H
 
+#include <string>
+using namespace std;
+
 class boolean {
     private:
         char* array;
         int size;
     
     public:
-        boolean(int size) {
-            // TODO
+        boolean(int size): array(NULL) {
+            int sizes = (size/8) + 1;
+            array = new char[sizes];
+            for (int i = 0; i < sizes; ++i)
+            {
+                array[i] = 0x00;
+            }
+            cout << sizes << "\n";
         }
         
         void bitOn(int position) {
-            // TODO
+            array[position/8] |= 1 << (position%8);
+            for (int i = 0; i < 8; ++i)
+            {
+                unsigned long bit = (array[position/8]>> i) & 1UL;
+                cout<<bit;
+            }
+            cout<<"\n";
+            
+
         }
              
         void bitOff(int position) {
-            // TODO
+            array[position/8] &= ~(1 << (position%8));
+            for (int i = 0; i < 8; ++i)
+            {
+                unsigned long bit = (array[position/8]>> i) & 1UL;
+                cout << bit;
+            }
+            cout<<"\n";
         }
              
         bool operator[](int position) {
-            // TODO
+            int mask = 0x01 << (position%8);
+            int mask1 = array[position/8];
+            mask &= mask1;
+            
+            if(mask == 0){
+                return false;
+            }
+            else
+                return true;
         }
     
         void clear() {    
-            // TODO
+            for (int i = 0; i < size; ++i)
+            {
+                bitOff(i);
+            }
         }
              
         ~boolean() {
-            // TODO
+            delete[] array;
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-
+#include "binary.cpp"
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
+    cout << "Binary Array Practice" << endl;
     cout << "===========================================================" << endl << endl;
+
+    boolean mb(8);
+    mb.bitOn(2);
+    cout<< mb[2]<<"\n";
+    mb.bitOff(2);
+    cout<< mb[2]<<"\n";
 
     system("read");
     return EXIT_SUCCESS;
